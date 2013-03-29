7ab771c3731308e93d53fcb5e3500bdeb3b1146a
Ryan Maskell <MaskellRyan@me.com>
2013-03-28 21:16:25 -0700

    Sources Page

--- about.php
+++ about.php
@@ -6,4 +6,24 @@
 
 	<header class="subpage"><h1>We're Awesome</h1><h2>About Us</h2></header> <!-- HEADER OF PAGE -->
 
+		<div class="box"> <!-- START CONTENT DRAWER -->
+
+		<article>
+			<section>
+				<p>Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in <a href="#">American crime</a>. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.</p>
+			</section>
+			<aside>
+				<h3>Favorites</h3>
+				<ul> <!-- YOU MAY ALSO USE A <p> TAG HERE -->
+					<li><a href="#">Running</a></li>
+					<li><a href="#">Jumping</a></li>
+					<li><a href="#">Climbing Trees</a></li>
+					<li><a href="#">Putting on makeup</a></li>
+					<li><a href="#">While I'm up there</a></li>
+				</ul>
+			</aside>
+			<div class="tab"></div> <!-- DON'T REMOVE THIS TAB ELEMENT -->
+		</article>
+	</div> <!-- END CONTENT DRAWER -->
+
 <?php include 'global/foot.php';?>
--- sources.php
+++ sources.php
@@ -6,4 +6,25 @@
 
 	<header class="subpage"><h1>Check it out</h1><h2>Our Sources</h2></header> <!-- HEADER OF PAGE -->
 
+	<div class="box sources">
+		<figure>
+			<h1>Indy City<span> by Flickr user <a href="#">Ryan Maskell</a></span></h1>
+			<img src="_/content/img/history/earlyyears1.jpg">
+		</figure>
+		<article></article>
+	</div>
+	<div class="box sources">
+		<figure>
+			<h1>Early Years<span> by Flickr user <a href="#">Ryan Maskell</a></span></h1>
+			<img src="_/content/img/history/earlyyears2.jpg">
+		</figure>
+		<article></article>
+	</div>
+	<div class="box sources">
+		<figure>
+			<h1>Early Years<span> by Flickr user <a href="#">Ryan Maskell</a></span></h1>
+			<img src="_/content/img/history/earlyyears3.jpg">
+		</figure>
+		<article></article>
+	</div>
 <?php include 'global/foot.php';?>
