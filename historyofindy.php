27fef066e005ba60d6deadc0a2bf52e2bc03944b
Elle Swanson <SFsGirl@Elles-MacBook-Air.local>
2013-03-30 16:40:20 -0700

    test from hannah

--- historyofindy.php
+++ historyofindy.php
@@ -25,7 +25,7 @@
 		</div>
 		<article id="drawer1" class="collapse">
 			<section>
-				<p>Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in <a href="#">American crime</a>. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.</p>
+				<p>Test from Hannah</p>
 			</section>
 			<aside>
 				<h3>Helpful Links</h3>
