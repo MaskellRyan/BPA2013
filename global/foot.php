a5cb8d39659018c92df84100022ada65d58ecb80
Ryan Maskell <MaskellRyan@me.com>
2013-03-30 02:04:55 -0700

    Functional, but only on Mobile

--- global/foot.php
+++ global/foot.php
@@ -10,7 +10,34 @@
 		}
 	</script>
 	<script src="_/js/bootstrap.js"></script>
+	<script src="_/js/swipe.js"></script>
 	<script src="_/js/hashgrid.js"></script>
 	<script src="_/js/script.js"></script>
+<script>
+
+// pure JS
+// var elem = document.getElementById('mySwipe');
+// window.mySwipe = Swipe(elem, {
+  // startSlide: 4,
+  // auto: 3000,
+  // continuous: true,
+  // disableScroll: true,
+  // stopPropagation: true,
+  // callback: function(index, element) {},
+  // transitionEnd: function(index, element) {}
+// });
+
+// with jQuery
+window.mySwipe = $('.swipe').Swipe().data('Swipe');
+
+</script>
+
+<!-- <div style='text-align:center;padding-top:20px;'>
+  
+  <button onclick='mySwipe.prev()'>prev</button> 
+  <button onclick='mySwipe.next()'>next</button>
+
+</div> -->
+
 </body>
 </html>
