685afb70220fd835b71d4fa66c80fa394f6c6d48
Ryan Maskell <MaskellRyan@me.com>
2013-03-29 23:57:47 -0700

    Contacts Page Links

--- contact.php
+++ contact.php
@@ -6,13 +6,21 @@
 
 	<header class="subpage"><h1>Stay in touch</h1><h2>Contact Us</h2></header> <!-- HEADER OF PAGE -->
 
-	<div class="box threecol contact blue"> <!-- START -->
+	<div class="box threecol contact green"> <!-- START -->
 		<article>
 			<div class="avatar img-circle"><img src="_/content/team/ashley.jpg"></div>
 			<section>
 				<h1>Ashley Chaffin</h1>
+				<h2>Designer</h2>
 				<p>Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in <a href="#">American crime</a>. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.</p>
 			</section>
+			<footer>
+				<ul>
+					<li><a href="http://www.twitter.com/aysiedesign"><img src="_/img/twitter.png"></a></li>
+					<li><a href="mailto:ashley@aysiedesign.com"><img alt="Ashley@AysieDesign.com" src="_/img/email.png"></a></li>
+					<li><a href="http://www.aysiedesign.com"><img src="_/img/aysie.png"></a></li>
+				</ul>
+			</footer>
 		</article>
 	</div> <!-- END -->
 	<div class="box threecol contact orange"> <!-- START -->
@@ -20,17 +28,33 @@
 			<div class="avatar img-circle"><img src="_/content/team/ryan.jpg"></div>
 			<section>
 				<h1>Ryan Maskell</h1>
+				<h2>Developer</h2>
 				<p>Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in <a href="#">American crime</a>. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.</p>
 			</section>
+			<footer>
+				<ul>
+					<li><a href="http://www.twitter.com/maskellmedia"><img src="_/img/twitter.png"></a></li>
+					<li><a href="mailto:Ryan@MaskellMedia.com"><img alt="Ryan@MaskellMedia.com" src="_/img/email.png"></a></li>
+					<li><a href="http://www.MaskellMedia.com/"><img src="_/img/maskellmedia.png"></a></li>
+				</ul>
+			</footer>
 		</article>
 	</div> <!-- END -->
-	<div class="box threecol contact green"> <!-- START -->
+	<div class="box threecol contact blue"> <!-- START -->
 		<article>
 			<div class="avatar img-circle"><img src="_/content/team/hannah.jpg"></div>
 			<section>
 				<h1>Hannah Shields</h1>
+				<h2>Writer</h2>
 				<p>Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in <a href="#">American crime</a>. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.</p>
 			</section>
+			<footer>
+				<ul>
+					<li><a href="http://www.twitter.com/"><img src="_/img/twitter.png"></a></li>
+					<li><a href="mailto:Hannah@Briosodesign.com"><img alt="Hannah@Briosodesign.com" src="_/img/email.png"></a></li>
+					<li><a href="http://www.briosodesign.com/"><img src="_/img/brioso.png"></a></li>
+				</ul>
+			</footer>
 		</article>
 	</div> <!-- END -->
 
