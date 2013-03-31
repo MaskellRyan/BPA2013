21ac459ccf70660655b3d74a6bfbf1c1e0a83d6f
Ryan Maskell <MaskellRyan@me.com>
2013-03-31 02:30:06 -0700

    Mobile nav works

--- global/nav.php
+++ global/nav.php
@@ -1,4 +1,4 @@
-	<nav>
+ 	<nav class="is-hidden">
 		<div class="logo"><a href="index.php">
 			<img src="_/img/indy-logo-ani.gif">
 			<img src="_/img/indy-logo-ani.png">
@@ -13,4 +13,34 @@
 		</ul>
 		<?php define("category", basename(dirname(__FILE__))); include 'global/footernav.html';?>
 	</nav>
-	<div id="container">
+	
+
+<div class="navbar-wrapper is-shown">
+      <!-- Wrap the .navbar in .container to center it within the absolutely positioned parent. -->
+      <div class="container">
+
+        <div class="navbar navbar-inverse">
+          <div class="navbar-inner">
+            <!-- Responsive Navbar Part 1: Button for triggering responsive navbar (not covered in tutorial). Include responsive CSS to utilize. -->
+            <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="brand" href="index.php"><img src="_/img/indy-logo.png"></a>
+            <!-- Responsive Navbar Part 2: Place all navbar contents you want collapsed withing .navbar-collapse.collapse. -->
+            <div class="nav-collapse collapse">
+				<ul class="nav">
+					<li><a class="color-red" href="historyofindy.php">History of Indy</a></li>
+					<li><a class="color-green" href="memorials.php">Memorials</a></li>
+					<li><a class="color-blue" href="artsandculture.php">Arts & Culture</a></li>
+					<li><a class="color-orange" href="historicalsites.php">Historical Sites</a></li>
+				</ul>
+            </div><!--/.nav-collapse -->
+          </div><!-- /.navbar-inner -->
+        </div><!-- /.navbar -->
+
+      </div> <!-- /.container -->
+</div><!-- /.navbar-wrapper -->
+
+<div id="container">
