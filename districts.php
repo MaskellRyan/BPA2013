240bd4f72dd0bf891affcf54e68d33dadfc06c93
Hannah Shields <hannah@Hannahs-iMac.local>
2013-03-31 20:42:51 -0600

    remove breaks- districts

--- districts.php
+++ districts.php
@@ -26,7 +26,7 @@
 		</div>
 		<article id="drawer1" class="collapse">
 			<section>
-				<p>Only a few miles from downtown Indianapolis is the art scene of Broad Ripple Village. Complete with live theatre, historic buildings and boutiques, these charming streets are full of the most original music stages and nightlife diversions.<br><br><br><br></p>
+				<p>Only a few miles from downtown Indianapolis is the art scene of Broad Ripple Village. Complete with live theatre, historic buildings and boutiques, these charming streets are full of the most original music stages and nightlife diversions.</p>
 			</section>
 			<aside>
 				<h3>Helpful Links</h3>
@@ -60,7 +60,7 @@
 		</div>
 		<article id="drawer2" class="collapse">
 			<section>
-				<p>Known as the arts and theatre district of Indianapolis is the famous Mass Ave, the city's most historic and oppidan communities. It's eclectic, trendy, and the place to be!<br><br><br></p>
+				<p>Known as the arts and theatre district of Indianapolis is the famous Mass Ave, the city's most historic and oppidan communities. It's eclectic, trendy, and the place to be!</p>
 
 			</section>
 			<aside>
@@ -91,7 +91,7 @@
 		</div>
 		<article id="drawer3" class="collapse">
 			<section>
-				<p>This European style village is a one-of-a-kind cultural district, with its quaint town square and central fountain. A blend of funky independence and architecture makes Fountain Square a choice for visitors and natives alike.<br><br><br><br><br></p>
+				<p>This European style village is a one-of-a-kind cultural district, with its quaint town square and central fountain. A blend of funky independence and architecture makes Fountain Square a choice for visitors and natives alike.</p>
 			</section>
 			<aside>
 				<h3>What To do</h3> <!-- YOU MAY ALSO USE A <ul> HERE -->
@@ -131,7 +131,7 @@
 		</div>
 		<article id="drawer4" class="collapse">
 			<section>
-				<p>Featuring the Circle Centre Mall, local boutiques, and luxury hotel accommodations, this district a fashonista's dream come true. The Wholesale Districts reputation for inviting hospitality makes it one of its visitor's admired place to stay awhile. <br><br><br></p>
+				<p>Featuring the Circle Centre Mall, local boutiques, and luxury hotel accommodations, this district a fashonista's dream come true. The Wholesale Districts reputation for inviting hospitality makes it one of its visitor's admired place to stay awhile. </p>
 			</section>
 			<aside>
 				<h3>Fun Fact!</h3> <!-- YOU MAY ALSO USE A <ul> HERE -->
@@ -162,7 +162,7 @@
 		</div>
 		<article id="drawer5" class="collapse">
 			<section>
-				<p>Jazz is the beat that moves this district. Rich in African American culture is the city's most historically rich community. Come for the Jazz, perfectly restored neighborhoods,  unique heritage and the Madame Walker Theatre Center. <br><br><br><br><br></p>
+				<p>Jazz is the beat that moves this district. Rich in African American culture is the city's most historically rich community. Come for the Jazz, perfectly restored neighborhoods,  unique heritage and the Madame Walker Theatre Center. </p>
 			</section>
 			<aside>
 				<h3>All That Jazz!</h3> <!-- YOU MAY ALSO USE A <ul> HERE -->
@@ -193,7 +193,7 @@
 		</div>
 		<article id="drawer6" class="collapse">
 			<section>
-				<p>Home of the Indianapolis Zoo, White River Gardens, Indiana State Museum, NCAA Hall of Champions, and Victory Field is the special district of Canal and White River State Park. The numerous attractions make this cultural hubbub a whole family favorite. There is something for everyone at any age.<br><br><br><br><br></p>
+				<p>Home of the Indianapolis Zoo, White River Gardens, Indiana State Museum, NCAA Hall of Champions, and Victory Field is the special district of Canal and White River State Park. The numerous attractions make this cultural hubbub a whole family favorite. There is something for everyone at any age.</p>
 			</section>
 			<aside>
 			<h3>Helpful Links</h3>
